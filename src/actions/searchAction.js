7838ac653dbabe936034df7c1cfaef47b5fa8aec
Ayoub Belomari <abelomar@e2r13p4.1337.ma>
2022-09-18 23:31:46 +0100

    user Details

--- src/actions/searchAction.js
+++ src/actions/searchAction.js
@@ -1,17 +1,32 @@
 import axios from "axios";
+require('dotenv').config();
 
 
 const githubApi = axios.create({
-    baseURL: "https://api.github.com/users/"
+    baseURL: "https://api.github.com/users/",
+    headers:{
+        'Authorization':`Bearer `
+    }
+
 })
 
 export const  searchByUserName  = async ({queryKey}) => {
 try {
 
     const response = await githubApi.get(`${queryKey[1]}/repos?sort=updated`);
-    return response;
+    const result = await githubApi.get(`${queryKey[1]}`);
+    return {response, result};
     
 } catch (error) {
     console.log(error);
 }
+}
+
+export const searchUserDetails = async  ({queryKey}) => {
+    try {
+    const response = await githubApi.get(`${queryKey[1]}/repos?sort=updated`);
+    return response;
+    } catch (error) {
+        console.log(error);
+    }
 }
